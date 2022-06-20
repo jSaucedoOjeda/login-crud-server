d025457f97a42674c523c61d8227765c380c0417
Axel Gudino <gudi_axel@hotmail.com>
2022-06-20 08:47:30 -0700

    commit axel

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -10,6 +10,6 @@ namespace login_crud_server
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string Summary { get; set; } 
     }
 }
